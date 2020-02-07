189cd12d560b04c271155c887307e8085ee53822
George <admin@georgesimos.com>
2020-02-08 00:54:29 +0200

    implementing custom map

--- map.js
+++ map.js
@@ -7,3 +7,24 @@ const makeBroken = function(item) {
 const brokenWeapons = weapons.map(weapon => makeBroken(weapon));
 console.log("is Array??? :", Array.isArray(brokenWeapons));
 console.log(brokenWeapons);
+
+// Custom map
+
+const _ = {};
+
+_.map = function(list, callback) {
+  // Looping through list
+  if (Array.isArray(list)) {
+    const clone = [];
+    for (let i = 0; i < list.length; i++) {
+      clone.push(callback(list[i], i, list)); // element, index, self
+    }
+    return clone;
+  }
+};
+
+console.log(
+  _.map([1, 2, 3], function(el) {
+    return el + 1;
+  })
+);
