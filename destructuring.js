6e9ae60348273d10899c0a0f1016e14c2e37e3ff
George <admin@georgesimos.com>
2020-02-07 20:59:40 +0200

    destructuring small example

---
+++ destructuring.js
@@ -0,0 +1,5 @@
+const [first, second] = [true, false]; // Creating values on the left ( Targets ) assign them from the right ( Source )
+console.log(first, second);
+
+const { one, two } = { one: 1, two: 2 };
+console.log(one, two);
