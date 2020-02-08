97cdb5d1cfcc4d23fd3e0c115fd58ddfe402e878
George <admin@georgesimos.com>
2020-02-08 11:11:42 +0200

    function Anatomy

---
+++ functionAnatomy.js
@@ -0,0 +1,6 @@
+var add = function(a, b) {
+  // add: Function name, a,b: Parameters, {}: Declaration / Definition
+  return a + b; // Function Body
+};
+
+add(3, 5); // Invocation/Call-time, 3,5: Arguments
