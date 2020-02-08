240ff97909c0e3c7167a0f2da5f463fbf8d58c67
George <admin@georgesimos.com>
2020-02-08 14:21:10 +0200

    more advance

--- closure.js
+++ closure.js
@@ -1,11 +1,28 @@
+// const myAlert = () => {
+//   const x = "Help! I think I found a clue!";
+//   const alerter = () => {
+//     console.log(x);
+//   };
+
+//   setTimeout(alerter, 1000);
+//   console.log("what happens first? this log or the alert?");
+// };
+
+// myAlert();
+
 const myAlert = () => {
   const x = "Help! I think I found a clue!";
+  let count = 0;
   const alerter = () => {
-    alert(x);
+    console.log(`${x} ${++count}`);
   };
 
-  setTimeout(alerter, 1000);
-  console.log("what happens first? this log or the alert?");
+  return alerter;
 };
 
-myAlert();
+const funcAlert = myAlert();
+const funcAlert2 = myAlert();
+funcAlert();
+funcAlert();
+funcAlert();
+funcAlert2(); // new context
