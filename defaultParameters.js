a7a102028a4dc2b9a9a5b26998fe4e5fc12f6b4b
George <admin@georgesimos.com>
2020-02-08 11:54:57 +0200

    Old way ES5

--- defaultParameters.js
+++ defaultParameters.js
@@ -5,3 +5,12 @@ const add = function(a, b = 2) {
 
 const sum = add(5);
 console.log(sum);
+
+// Old way ES5
+const oldAdd = function(a, b) {
+  b = b || 2; // if b is not exxist then assign to it, 2
+  console.log(arguments);
+  return a + b;
+};
+
+const oldSum = oldAdd(5);
