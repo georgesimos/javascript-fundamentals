dd622c9fba3db52878f07344520cdf171e4b58b6
George <admin@georgesimos.com>
2020-02-08 14:24:56 +0200

    new Clue

--- closure.js
+++ closure.js
@@ -10,19 +10,34 @@
 
 // myAlert();
 
-const myAlert = () => {
-  const x = "Help! I think I found a clue!";
-  let count = 0;
-  const alerter = () => {
-    console.log(`${x} ${++count}`);
-  };
+// const myAlert = () => {
+//   const x = "Help! I think I found a clue!";
+//   let count = 0;
+//   const alerter = () => {
+//     console.log(`${x} ${++count}`);
+//   };
 
-  return alerter;
+//   return alerter;
+// };
+
+// const funcAlert = myAlert();
+// const funcAlert2 = myAlert();
+// funcAlert();
+// funcAlert();
+// funcAlert();
+// funcAlert2(); // new context
+
+const newClue = name => {
+  const length = name.length;
+
+  return weapon => {
+    let clue = length + weapon.length;
+    return !!(clue % 2);
+  };
 };
 
-const funcAlert = myAlert();
-const funcAlert2 = myAlert();
-funcAlert();
-funcAlert();
-funcAlert();
-funcAlert2(); // new context
+const didGreenDoItWith = newClue("Green");
+console.log(didGreenDoItWith("knife"));
+console.log(didGreenDoItWith("gun"));
+console.log(didGreenDoItWith("tv"));
+console.log("Wow he did it with a tv ...");
