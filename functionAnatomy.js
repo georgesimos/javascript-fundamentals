cf099191cd2c61dbc5e574891bec6974d81a64e9
George <admin@georgesimos.com>
2020-02-08 11:16:40 +0200

    Definition

--- functionAnatomy.js
+++ functionAnatomy.js
@@ -5,5 +5,6 @@ var add = function(a, b) {
 
 add(3, 5); // Invocation/Call-time, 3,5: Arguments
 
+// What is Definition ??? If you see a function and {} this is a Definition
 // Parameters are variables that do not have a value until the called
 // Arguments are the actual values.
