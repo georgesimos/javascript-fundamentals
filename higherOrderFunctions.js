c05eaf685cf385d189cb528947e89b8a1d863673
George <admin@georgesimos.com>
2020-02-08 13:39:17 +0200

    Passing Arguments

--- higherOrderFunctions.js
+++ higherOrderFunctions.js
@@ -11,3 +11,17 @@ const result = ifElse(
   () => console.log(false)
 );
 console.log(result());
+
+// Passing Arguments
+var increments = function(n) {
+  return n + 1;
+};
+
+var square = function(n) {
+  return n * n;
+};
+
+var doMathSoIDontHaveTo = function(n, func) {
+  return func(n);
+};
+console.log(doMathSoIDontHaveTo(5, square), doMathSoIDontHaveTo(4, increments));
