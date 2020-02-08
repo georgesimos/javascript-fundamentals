7e4f47b8b2755a70b222bf5a5a3d4ee69c87aebf
George <admin@georgesimos.com>
2020-02-08 12:02:45 +0200

    Array.from

--- arrayLikeObject.js
+++ arrayLikeObject.js
@@ -1,5 +1,14 @@
+// const constructArr = function() {
+//   const arr = Array.prototype.slice.call(arguments);
+//   arr.push("the billliards room?");
+//   return arr.join(" ");
+// };
+
+// const newArray = constructArr("was", "it", "in");
+// console.log(newArray);
+
 const constructArr = function() {
-  const arr = Array.prototype.slice.call(arguments);
+  const arr = Array.from(arguments); // Creating an array from arguments
   arr.push("the billliards room?");
   return arr.join(" ");
 };
